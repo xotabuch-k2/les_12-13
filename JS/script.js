7253c9d7e03edc6cdf15c0069bf987447ba23193
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-02-14 15:44:18 +0200

    hw_20

--- JS/script.js
+++ JS/script.js
@@ -20,16 +20,52 @@
 // alert(`Your full name is: ${userName} ${userSurname}, and you are ${userData}`)
 // let userAdmin = confirm('You is admin?');
 // alert(`Your full name is: ${userName} ${userSurname}, and you are ${userData}, admin statys ${userAdmin}`)
+///////////////////////////////////////////////////
+// let mon1 = prompt(" Enter 1 month", );
+// typeof mon1 === "number";
+// let mon2 = prompt("Enter 2 month", );
+// typeof mon2 === "number";
+// let hw_1 = +mon1 + +mon2;
+// alert(`Your salary is: ${hw_1}$`);
+// let hw_2 = hw_1;
+// ++hw_2;
+// alert(`Your salary is ${hw_1} +1$ is:${hw_2}$`);
+// rez = (hw_2>=2000);
+// alert(`Will you work next? ${rez}`)
+// alert(rez && "I\'m ready to work" || !rez && "I\'m out");
+///////////////////////////////////////////////////
 
-let mon1 = prompt(" Enter 1 month", );
-typeof mon1 === "number";
-let mon2 = prompt("Enter 2 month", );
-typeof mon2 === "number";
-let hw_1 = +mon1 + +mon2;
-alert(`Your salary is: ${hw_1}$`);
-let hw_2 = hw_1;
-++hw_2;
-alert(`Your salary is ${hw_1} +1$ is:${hw_2}$`);
-rez = (hw_2>=2000);
-alert(`Will you work next? ${rez}`)
-alert(rez && "I\'m ready to work" || !rez && "I\'m out");
+
+
+
+/////1HW///////////////////////////////////////////
+// let age = prompt(" Enter your age", );
+// if (age<17) {
+//     confirm("Do you stadu at school?")
+// } else if (age>17 && age<25) {
+//     confirm("Do you stadu at university?")
+// } else if (age>=25 && age<60) {
+//     confirm("Do you have a jobe?")
+// } else (confirm("What are you doing?"))
+/////2HW///////////////////////////////////////////
+let style = prompt(" Enter CSS Display style: \n1-block; \n2-flex; \n3-grid; \n4-inline; \n5-none.")
+switch (style) {
+    case style="block":
+        alert('Display: block');
+        break;
+    case style='flex':
+        alert('Display: flex');
+        break;
+    case style='grid':
+        alert('Display: grid');
+        break;
+    case style='inline':
+        alert('Display: inline');
+        break;
+    case style='none':
+        alert('Display: none');
+        break;
+    default:
+        alert('Set \'display: inline-block;\'');
+}
+/////3HW///////////////////////////////////////////
