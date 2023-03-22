1d7c6a4403857c5d9b85b1ef7c6faffe163d0639
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-03-22 18:57:27 +0200

    hw_31

--- JS/dist/script.dev.js
+++ JS/dist/script.dev.js
@@ -784,3 +784,42 @@ fetchData(url).then(function (data) {
 })["catch"](function (error) {
   console.error(error);
 }); /////2HW///////////////////////////////////////////
+
+function getData(url) {
+  var result, data, filteredData;
+  return regeneratorRuntime.async(function getData$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(fetch(url));
+
+        case 3:
+          result = _context.sent;
+          _context.next = 6;
+          return regeneratorRuntime.awrap(result.json());
+
+        case 6:
+          data = _context.sent;
+          filteredData = data.filter(function (item) {
+            return item.title.startsWith('a');
+          });
+          console.log(filteredData);
+          _context.next = 14;
+          break;
+
+        case 11:
+          _context.prev = 11;
+          _context.t0 = _context["catch"](0);
+          console.error(_context.t0);
+
+        case 14:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0, 11]]);
+}
+
+getData('https://jsonplaceholder.typicode.com/todos');
--- JS/script.js
+++ JS/script.js
@@ -793,3 +793,41 @@ fetchData(url)
     console.error(error);
   });
 /////2HW///////////////////////////////////////////
+async function getData(url) {
+  try {
+    let result  = await fetch(url);
+    let data = await result.json();
+    const filteredData = data.filter(item => item.title.startsWith('a'));
+    console.log(filteredData);
+  } 
+  catch (error) {
+    console.error(error);
+  }
+}
+/////3HW///////////////////////////////////////////
+async function getData(url) {
+  try {
+    let result  = await fetch(url);
+    let data = await result.json();
+
+    let filteredDataA = data.filter(item => item.title.startsWith('a'));
+    console.log(filteredDataA);
+
+    let filteredDataAB = data.filter(item => item.title.startsWith('ab'));
+    console.log(filteredDataAB);
+
+    let myJsonA = JSON.stringify(filteredDataA);
+    myJsonA = filteredDataA.map(item => JSON.stringify(item)).join('<br>');
+    let userDivA = document.querySelector('.user-a');
+    userDivA.innerHTML = myJsonA;
+    let myJsonAB = JSON.stringify(filteredDataAB);
+    myJsonAB = filteredDataAB.map(item => JSON.stringify(item)).join('<br>');
+    let userDivAB = document.querySelector('.user-ab');
+    userDivAB.innerHTML = myJsonAB;
+       
+      } 
+
+  catch (error) {
+    console.error(error);
+  }
+}
