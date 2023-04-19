7657ae87eb78ab0a44485c2bc71db1e6bfebf47b
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-04-19 13:45:30 +0300

    hw_44

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import React, { useState, useEffect } from "react";
 
-  function App () {
-const [products, setProducts] = useState({
-  coffe: 0,
-  sugar: 0,
-})
+function App() {
+  const [products, setProducts] = useState({
+    coffe: 0,
+    sugar: 0,
+  });
 
+  const [showRemoveCoffe, setShowRemoveCoffe] = useState(false);
+  const [showRemoveSugar, setShowRemoveSugar] = useState(false);
 
-const addCoffe = () => setProducts((prevState) => {
-  return {
-    ...prevState,
-    coffe: prevState.coffe + 1,
-  }
-});
-const addSugar = () => setProducts((prevState) => {
-  return {
-    ...prevState,
-    sugar: prevState.sugar + 1,
-  }
-});
-const subCoffe = () => {
-  if (products.coffe > 0) {
+  const addCoffe = () =>
     setProducts((prevState) => {
       return {
         ...prevState,
-        coffe: prevState.coffe - 1,
+        coffe: prevState.coffe + 1,
       };
     });
-  }
-};
-const subSugar = () => {
+  const addSugar = () =>
+    setProducts((prevState) => {
+      return {
+        ...prevState,
+        sugar: prevState.sugar + 1,
+      };
+    });
+
+  const removeCoffe = () => {
+    if (products.coffe > 0) {
+      setProducts((prevState) => {
+        return {
+          ...prevState,
+          coffe: prevState.coffe - 1,
+        };
+      });
+    }
+  };
+  const removeSugar = () => {
     if (products.sugar > 0) {
       setProducts((prevState) => {
         return {
@@ -39,45 +44,58 @@ const subSugar = () => {
       });
     }
   };
-const save = () => {
-  localStorage.setItem('coffe', coffe);
-  localStorage.setItem('sugar', sugar);
-}
 
-const clear = () => {
-    localStorage.removeItem('coffe');
-    localStorage.removeItem('sugar');
-    setCoffe(0);
-    setSugar(0);
-}
+  const save = () => {
+    localStorage.setItem("coffe", products.coffe);
+    localStorage.setItem("sugar", products.sugar);
+  };
 
-useEffect(()=>{
-  if (localStorage.getItem('coffe')){
-    setCoffe(+localStorage.getItem('coffe'));
-    setSugar(+localStorage.getItem('sugar'));
+  const clear = () => {
+    localStorage.removeItem("coffe");
+    localStorage.removeItem("sugar");
+    setProducts({
+      coffe: 0,
+      sugar: 0,
+    });
+  };
 
-  }
-}, []);
-return (
-  <div className="wrapper">
-    <div className="list">
-      <h1>Product list</h1>
-      <div className='product'>
-      <span>{`Coffe: ${products.coffe}`}</span>
-      <button onClick={addCoffe}>Add</button>
-      <button onClick={subCoffe}>Remove</button>
-      </div>
-      <div className='product'>
-      <span>{`Sugar: ${products.sugar}`}</span>
-        <button onClick={addSugar}>Add</button>
-        <button onClick={subSugar}>Remove</button>
-      </div>
-      <div className='save'>
+  useEffect(() => {
+    if (localStorage.getItem("coffe")) {
+      setProducts((prevState) => {
+        return {
+          ...prevState,
+          coffe: +localStorage.getItem("coffe"),
+          sugar: +localStorage.getItem("sugar"),
+        };
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    setShowRemoveCoffe(products.coffe === 0 ? false : true);
+    setShowRemoveSugar(products.sugar === 0 ? false : true);
+  }, [products.coffe, products.sugar]);
+
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h1>Product list</h1>
+        <div className="product">
+          <span>{`Coffe: ${products.coffe}`}</span>
+          <button onClick={addCoffe}>Add</button>
+          {showRemoveCoffe && <button onClick={removeCoffe}>Remove</button>}
+        </div>
+        <div className="product">
+          <span>{`Sugar: ${products.sugar}`}</span>
+          <button onClick={addSugar}>Add</button>
+          {showRemoveSugar && <button onClick={removeSugar}>Remove</button>}
+        </div>
+        <div className="save">
           <button onClick={save}>SAVE</button>
           <button onClick={clear}>CLEAR</button>
         </div>
+      </div>
     </div>
-  </div> 
   );
 }
 
