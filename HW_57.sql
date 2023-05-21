66660e433f17361bedb9e9f0f10cd72d1a094c64
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-05-21 22:41:15 +0300

    hw_57

--- HW_57.sql
+++ HW_57.sql
@@ -2,30 +2,30 @@ mysql> CREATE DATABASE DB_hw_57;
 mysql> Use DB_hw_57;
 mysql> CREATE TABLE DB_hw_57 (Product VARCHAR(128),Price DECIMAL(5,0),rating DECIMAL(3,2), category VARCHAR(16), brand VARCHAR(17));
 mysql> ALTER TABLE DB_hw_57 ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY;
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('iPhone 9','549','4.69','smsrtphones','Apple');
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('iPhone X','899','4.44','smsrtphones','Apple');
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Samsyng Universe 9','1249','4.09','smsrtphones','Samsung');
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('OPPO F19','280','4.3','smsrtphones','OPPO');
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Huawei P30','499','4.09','smsrtphones','Huawei');
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('MacBook Pro','1149','4.57','leptops','Apple');
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Samsyng Galaxy Book','1499','4.25','leptops','Samsung');
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Microsoft Surface Laptop 4','1499','4.43','leptops','Microsoft Surface');
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Infinix INBOOK','1099','4.43','leptops','Infinix');
-mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('HP Pavilion 15-DK1056WM','1099','4.54','leptops','HP Pavilion');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('iPhone 9','549','4.69','smartphones','Apple');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('iPhone X','899','4.44','smartphones','Apple');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Samsung Universe 9','1249','4.09','smartphones','Samsung');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('OPPO F19','280','4.3','smartphones','OPPO');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Huawei P30','499','4.09','smartphones','Huawei');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('MacBook Pro','1149','4.57','laptops','Apple');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Samsung Galaxy Book','1499','4.25','laptops','Samsung');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Microsoft Surface Laptop 4','1499','4.43','laptops','Microsoft Surface');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('Infinix INBOOK','1099','4.43','laptops','Infinix');
+mysql> INSERT INTO DB_hw_57(Product, Price, rating, category, brand) VALUES('HP Pavilion 15-DK1056WM','1099','4.54','laptops','HP Pavilion');
 mysql> SELECT * FROM DB_hw_57;
 +----------------------------+-------+--------+-------------+-------------------+----+
 | Product                    | Price | rating | category    | brand             | id |
 +----------------------------+-------+--------+-------------+-------------------+----+
-| iPhone 9                   |   549 |   4.69 | smsrtphones | Apple             |  1 |
-| iPhone X                   |   899 |   4.44 | smsrtphones | Apple             |  2 |
-| Samsyng Universe 9         |  1249 |   4.09 | smsrtphones | Samsung           |  3 |
-| OPPO F19                   |   280 |   4.30 | smsrtphones | OPPO              |  4 |
-| Huawei P30                 |   499 |   4.09 | smsrtphones | Huawei            |  5 |
-| MacBook Pro                |  1149 |   4.57 | leptops     | Apple             |  6 |
-| Samsyng Galaxy Book        |  1499 |   4.25 | leptops     | Samsung           |  7 |
-| Microsoft Surface Laptop 4 |  1499 |   4.43 | leptops     | Microsoft Surface |  8 |
-| Infinix INBOOK             |  1099 |   4.43 | leptops     | Infinix           |  9 |
-| HP Pavilion 15-DK1056WM    |  1099 |   4.54 | leptops     | HP Pavilion       | 10 |
+| iPhone 9                   |   549 |   4.69 | smartphones | Apple             |  1 |
+| iPhone X                   |   899 |   4.44 | smartphones | Apple             |  2 |
+| Samsung Universe 9         |  1249 |   4.09 | smartphones | Samsung           |  3 |
+| OPPO F19                   |   280 |   4.30 | smartphones | OPPO              |  4 |
+| Huawei P30                 |   499 |   4.09 | smartphones | Huawei            |  5 |
+| MacBook Pro                |  1149 |   4.57 | laptops     | Apple             |  6 |
+| Samsung Galaxy Book        |  1499 |   4.25 | laptops     | Samsung           |  7 |
+| Microsoft Surface Laptop 4 |  1499 |   4.43 | laptops     | Microsoft Surface |  8 |
+| Infinix INBOOK             |  1099 |   4.43 | laptops     | Infinix           |  9 |
+| HP Pavilion 15-DK1056WM    |  1099 |   4.54 | laptops     | HP Pavilion       | 10 |
 +----------------------------+-------+--------+-------------+-------------------+----+
 10 rows in set (0.00 sec)
 mysql> ALTER TABLE DB_hw_57 ADD INDEX(Product(20));
@@ -61,8 +61,8 @@ mysql> SELECT Product FROM DB_hw_57 WHERE Price BETWEEN "500" AND "1500";
 | Infinix INBOOK             |
 | HP Pavilion 15-DK1056WM    |
 | MacBook Pro                |
-| Samsyng Universe 9         |
-| Samsyng Galaxy Book        |
+| Samsung Universe 9         |
+| Samsung Galaxy Book        |
 | Microsoft Surface Laptop 4 |
 +----------------------------+
 8 rows in set (0.00 sec)
@@ -75,15 +75,15 @@ mysql> SELECT Product, Price,rating FROM DB_hw_57 WHERE brand IN ("Apple", "Sams
 | iPhone X            |   899 |   4.44 |
 | MacBook Pro         |  1149 |   4.57 |
 | Huawei P30          |   499 |   4.09 |
-| Samsyng Universe 9  |  1249 |   4.09 |
-| Samsyng Galaxy Book |  1499 |   4.25 |
+| Samsung Universe 9  |  1249 |   4.09 |
+| Samsung Galaxy Book |  1499 |   4.25 |
 +---------------------+-------+--------+
 6 rows in set (0.00 sec)
-mysql> SELECT Product, category FROM DB_hw_57 WHERE Product LIKE "Samsyng%";
+mysql> SELECT Product, category FROM DB_hw_57 WHERE Product LIKE "Samsung%";
 +---------------------+-------------+
 | Product             | category    |
 +---------------------+-------------+
-| Samsyng Galaxy Book | leptops     |
-| Samsyng Universe 9  | smsrtphones |
+| Samsung Galaxy Book | laptops     |
+| Samsung Universe 9  | smartphones |
 +---------------------+-------------+
 2 rows in set (0.00 sec)
