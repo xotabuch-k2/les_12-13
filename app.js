cfe9973ea1335d512574e415529755d5d8dadee4
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-05-21 22:35:08 +0300

    hw_60

---
+++ app.js
@@ -0,0 +1,6 @@
+
+
+const newData = [
+    { name: 'Anna', age: 24 },
+    { name: 'Tom', age: 21 },
+  ];
---
+++ dist/app.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var newData = [{
+  name: 'Anna',
+  age: 24
+}, {
+  name: 'Tom',
+  age: 21
+}];
