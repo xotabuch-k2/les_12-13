d7613886f996e8f7adde6697ed80533b4b8c5791
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-05-29 18:01:07 +0300

    Fin_Progect

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -2,24 +2,20 @@ import { useState } from 'react'
 import '../scss/App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(666)
 
   return (
     <>
       <div>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Рот + Эбал</h1>
+      <h2>РотЭбал этого проекта</h2>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
