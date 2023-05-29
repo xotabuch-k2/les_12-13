284348c36c38b2727f9c5f3ad051d50933b6355f
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-05-29 17:11:04 +0300

    Fin_Progect

--- dist/index.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _express = _interopRequireDefault(require("express"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var port = 8000;
-var app = (0, _express["default"])();
