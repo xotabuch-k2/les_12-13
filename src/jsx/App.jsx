a344eafa8898959c3498cf21a50b73a247b580ce
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-06-02 14:03:21 +0300

    hw_64

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -24,9 +24,11 @@ function App() {
       <section className='section1'>
         <div className='textSection1'>
           <h1 className='textSection1_h1'> <span className='textSection1_h1_s1'>Dive</span>  Into The Depths <br /> Of <span className='textSection1_h1_s2'>Virtual Reality</span></h1>
-          <p className='textSection1_h2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />
-          sed do eiusmod tempor incididunt ut labore et dolore <br />
-          nisl tincidunt eget. Lectus mauris eros in vitae.</p>
+          <p className='textSection1_h2'>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />
+sed do eiusmod tempor incididunt ut labore et dolore <br />
+nisl tincidunt eget. Lectus mauris eros in vitae.
+          </p>
           <div className='imgButt'>
           <button className='imgButtA' href="#"> BUILD YOUR WORLD</button>
             <img src="/public/img/1 block/arrow-small-right.svg" alt="" />
@@ -44,25 +46,51 @@ function App() {
       </section>
       <section className='secInfo'>
         <div className='secInfoL'>
-          <div>
-          <img src="/public/img/1 block/location.svg" alt="" />
+          <div className='secInfoLimg'>
+            <img src="/public/img/1 block/location.svg" alt="" />
           </div>
-          <div>
-          <h3>Pay Us a Visit</h3>
-          <h4>Union St, Seattle, WA 98101, United States</h4>
+          <div className='secInfoLtext'>
+            <h3 className='secInfoLtextU'>Pay Us a Visit</h3>
+            <h4 className='secInfoLtextD'>Union St, Seattle, WA 98101, United States</h4>
           </div>
-
         </div>
-        <div className='secInfoLine'></div>
+        <div className='secInfoLine1'></div>
         <div className='secInfoC'>
-
+          <div className='secInfoCimg'>
+          <img src="/public/img/1 block/phone-call.svg" alt="" />
+          </div>
+          <div className='secInfoCtext'>
+            <h3 className='secInfoCtextU'>Give Us a Call</h3>
+            <h4 className='secInfoCtextD'>(110) 1111-1010</h4>
+          </div>
         </div>
-        <div className='secInfoLine'></div>
-        <div className='secInfoR'>
+        <div className='secInfoLine2'></div>
 
+        <div className='secInfoR'>
+        <div className='secInfoRimg'>
+          <img src="/public/img/1 block/mail.svg" alt="" />
+          </div>
+          <div className='secInfoRtext'>
+            <h3 className='secInfoRtextU'>Send Us a Message</h3>
+            <h4 className='secInfoRtextD'>Contact@HydraVTech.com</h4>
+          </div>
         </div>
       </section>
-      <section></section>
+      <section className='introText'>
+        <div className='intro'>
+          <h2>INTRODUCTION</h2>
+          <h3>TO HYDRA VR</h3>
+        </div>
+        <img src="/public/img/2 block/arrow-small-right.svg" alt="" />
+        <div className='text'>
+          <p>
+Vitae sapien pellentesque habitant morbi tristique senectus et netus et. Feugiat 
+nibh sed pulvinar proin gravida hendrerit lectus. Mi sit amet mauris commodo 
+quis imperdiet massa tincidunt nunc. Viverra aliquet eget sit amet tellus. Ornare 
+lectus sit amet est placerat in. Lectus magna fringilla urna porttitor rhoncus vitae.
+          </p>
+        </div>
+      </section>
       <section></section>
       <section></section>
       <section></section>      
