27cc7439c5f2cde378919c453a1ca2b862b4a3ea
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-06-06 09:58:05 +0300

    hw_64

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -4,13 +4,17 @@ var _express = _interopRequireDefault(require("express"));
 
 var _mongoose = _interopRequireDefault(require("mongoose"));
 
-var _customers = require("./model/customers.js");
+require("dotenv/config");
+
+var _product = require("./model/product.js");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var PORT = 3000;
-var url = '';
+var url = 'mongodb+srv://xotabuch-k2:Nfhenbyj68@learn.7mjqj3b.mongodb.net/';
 var app = (0, _express["default"])();
+app.use(_express["default"]["static"]('css'));
+app.set('view engine', 'pug');
 
 _mongoose["default"].connect(url).then(function () {
   console.log('Connected to DB');
@@ -22,11 +26,11 @@ _mongoose["default"].connect(url).then(function () {
 });
 
 app.get('/', function (req, res) {
-  _customers.Customer.find().populate('product_id').then(function (customers) {
-    var productsHtml = customers.map(function (customer) {
-      return "\n          <div style=\"border: 1px solid #000; width: fit-content; margin: 0 0 20px 0; padding: 0 10px\">\n            <span>".concat(customer.name, ":</span> <span style=\"padding: 0 10px\">").concat(customer.product_id.title, "</span>  <span>Price: ").concat(customer.product_id.price, "</span>\n          </div>\n        ");
+  _product.Product.find().then(function (products) {
+    var productsHtml = products.map(function (product) {
+      return "\n        <div style=\"border: 1px solid #000; \n        width: fit-content; \n        margin: 0 0 20px 0; \n        padding: 0 10px\">\n          <h2>".concat(product.title, "</h2>\n          <p>Price: ").concat(product.price, "</p>\n        </div>\n                ");
     });
-    var html = "<h1>Users purchases:</h1> ".concat(productsHtml.join(''));
+    var html = "<h1>Products:</h1> ".concat(productsHtml.join(''));
     res.send(html);
   })["catch"](function (error) {
     console.error(error);
--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-import express from 'express';
+import express from 'express'
 import mongoose from 'mongoose';
-import { Customer } from './model/customers.js';
+import 'dotenv/config'
+import { Product } from './model/product.js';
 
 const PORT = 3000;
-const url = '';
+const url = 'mongodb+srv://xotabuch-k2:Nfhenbyj68@learn.7mjqj3b.mongodb.net/';
 const app = express();
+app.use(express.static('css'));
+app.set('view engine', 'pug');
 
 mongoose.connect(url)
-  .then(() => {
-    console.log('Connected to DB');
-    app.listen(PORT, () => {
-      console.log(`Server started on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(`DB connection error: ${err}`);
-  });
+        .then(()=> {
+            console.log('Connected to DB');
+            app.listen(PORT, ()=> {
+                console.log(`Server started on http://localhost:${PORT}`);
+            })
+        })
+        .catch((err)=> {console.log(`DB connection error: ${err}`)});
 
-  app.get('/', (req, res) => {
-    Customer.find()
-      .populate('product_id')
-      .then((customers) => {
-        const productsHtml = customers.map((customer) => `
-          <div style="border: 1px solid #000; width: fit-content; margin: 0 0 20px 0; padding: 0 10px">
-            <span>${customer.name}:</span> <span style="padding: 0 10px">${customer.product_id.title}</span>  <span>Price: ${customer.product_id.price}</span>
-          </div>
-        `);
-  
-        const html = `<h1>Users purchases:</h1> ${productsHtml.join('')}`;
-        res.send(html);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
+        app.get('/', (req, res) => {
+            Product.find()
+                .then(products => {
+                const productsHtml = products.map(product => `
+        <div style="border: 1px solid #000; 
+        width: fit-content; 
+        margin: 0 0 20px 0; 
+        padding: 0 10px">
+          <h2>${product.title}</h2>
+          <p>Price: ${product.price}</p>
+        </div>
+                `);
+                const html = `<h1>Products:</h1> ${productsHtml.join('')}`;
+                res.send(html);
+                })
+                .catch(error => {
+                console.error(error);
+                });
+            });
+        
