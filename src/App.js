754110128258d8e125288f73aa102e9b037dbe76
Vyacheslav_Mayboroda <slavik412oe@gmail.com>
2023-06-09 12:31:02 +0300

    hw_47_3

--- src/App.js
+++ src/App.js
@@ -40,15 +40,19 @@ function App() {
       <Formik initialValues={initialValues} onSubmit={handleSubmit} validate={validate}>
         {({values, errors, touched, handleChange, handleBlur, handleSubmit}) => (
           <Form onSubmit={handleSubmit}>
-            <label>Name</label>
+
+            <label placeholder="Enter Name">Name</label>
             <Field className={touched.name && errors.name ? "invalid" : ""} name="name" onChange={handleChange} onBlur={handleBlur} />
             {touched.name && errors.name && <p>{errors.name}</p>}
+
             <label>Login</label>
             <Field className={touched.login && errors.login ? "invalid" : ""} name="login" onChange={handleChange} onBlur={handleBlur} />
             {touched.login && errors.login && <p>{errors.login}</p>}
+
             <label>Age</label>
             <Field className={touched.age && errors.age ? "invalid" : ""} name="age" type="number" onChange={handleChange} onBlur={handleBlur} />
             {touched.age && errors.age && <p>{errors.age}</p>}
+            
             <label>Email</label>
             <Field className={touched.email && errors.email ? "invalid" : ""} name="email" onChange={handleChange} onBlur={handleBlur} />
             {touched.email && errors.email && <p>{errors.email}</p>}
